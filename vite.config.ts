96bd5576adaf46ab1971106d993d05b9ed17b4d5
Andres Court <andres@andrescourt.com>
2025-09-25 17:50:29 -0500

    netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
-    plugins: [tsConfigPaths(), tanstackStart(), viteReact()],
+    plugins: [tsConfigPaths(), tanstackStart(), netlify(), viteReact()],
 });
