651f2883b500f7e5c9f18748d8908d95fac54a90
Andres Court <andres@andrescourt.com>
2025-09-25 17:43:09 -0500

    initial commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,52 @@
+/// <reference types="vite/client" />
+
+import type { ReactNode } from "react";
+import {
+    createRootRoute,
+    HeadContent,
+    Outlet,
+    Scripts,
+} from "@tanstack/react-router";
+
+export const Route = createRootRoute({
+    head: () => ({
+        meta: [
+            {
+                charSet: "utf-8",
+                lang: "en",
+            },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+            {
+                title: "Tanstack Start",
+            },
+        ],
+    }),
+    component: RootComponent,
+});
+
+function RootComponent() {
+    return (
+        <RootDocument>
+            <Outlet />
+        </RootDocument>
+    );
+}
+
+function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
+    return (
+        // biome-ignore lint/a11y/useHtmlLang: defined in the meta properties of the head object
+        <html>
+            <head>
+                <HeadContent />
+            </head>
+
+            <body>
+                {children}
+                <Scripts />
+            </body>
+        </html>
+    );
+}
