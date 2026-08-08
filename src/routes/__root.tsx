74861436d4c606b9830cb831ce87eb288f1a7939
Andres Court <andres@andrescourt.com>
2026-08-07 21:05:32 -0500

    feat(biome): fix linting and formatting

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,29 +1,29 @@
-import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
-import type { ReactNode } from "react";
-import appCss from "../styles.css?url";
+import { createRootRoute, HeadContent, Scripts } from '@tanstack/react-router'
+import type { ReactNode } from 'react'
+import appCss from '../styles.css?url'
 
 export const Route = createRootRoute({
 	head: () => ({
 		meta: [
-			{ charSet: "utf-8" },
-			{ name: "viewport", content: "width=device-width, initial-scale=1" },
-			{ title: "Chingu Tanstack Start" },
+			{ charSet: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ title: 'Chingu Tanstack Start' },
 		],
-		links: [{ rel: "stylesheet", href: appCss }],
+		links: [{ rel: 'stylesheet', href: appCss }],
 	}),
 	shellComponent: ShellComponent,
-});
+})
 
 function ShellComponent({ children }: Readonly<{ children: ReactNode }>) {
 	return (
-		<html lang="en">
+		<html lang='en'>
 			<head>
 				<HeadContent />
 			</head>
-			<body className="bg-green-200">
+			<body className='bg-green-200'>
 				{children}
 				<Scripts />
 			</body>
 		</html>
-	);
+	)
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,15 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "@/components/ui/button";
+import { createFileRoute } from '@tanstack/react-router'
+import { Button } from '@/components/ui/button'
 
-export const Route = createFileRoute("/")({
+export const Route = createFileRoute('/')({
 	component: RouteComponent,
-});
+})
 
 function RouteComponent() {
 	return (
 		<div>
 			<p>Hello "/"!</p>
-			<Button variant="default">Shadcn/UI button</Button>
+			<Button variant='default'>Shadcn/UI button</Button>
 		</div>
-	);
+	)
 }
