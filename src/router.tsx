37d479ef2bac7700310bc14a29a7b8a232b077ee
Andres Court <andres@andrescourt.com>
2026-08-07 21:39:40 -0500

    added default error and not found components

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,13 @@ export function getRouter() {
 	const router = createRouter({
 		routeTree,
 		scrollRestoration: true,
+		defaultErrorComponent: ({ error }) => (
+			<div>
+				<h1 className='text-2xl'>My custom error</h1>
+				{error.message}
+			</div>
+		),
+		defaultNotFoundComponent: () => <div>My custom not found</div>,
 	})
 
 	return router
