12283c7cb2608997abf95095aba8d63d6fa1d411
Andres Court <andres@andrescourt.com>
2026-08-07 20:54:30 -0500

    feat(Shadcn): use the button

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,15 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
 
-export const Route = createFileRoute('/')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/")({
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+	return (
+		<div>
+			<p>Hello "/"!</p>
+			<Button variant="default">Shadcn/UI button</Button>
+		</div>
+	);
 }
