519e5b250115b4e17a5923d9045e31f1157840dd
Andres Court <andres@andrescourt.com>
2026-08-07 21:10:35 -0500

    added navigation menu

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,9 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/about')({
-  component: RouteComponent,
+	component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/about"!</div>
+	return (
+		<div>
+			<ul className='flex gap-4'>
+				<li>
+					<Link to='/'>Home</Link>
+				</li>
+				<li>
+					<Link to='/about'>About</Link>
+				</li>
+			</ul>
+			Hello "/about"!
+		</div>
+	)
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { Button } from '@/components/ui/button'
 
 export const Route = createFileRoute('/')({
@@ -8,6 +8,17 @@ export const Route = createFileRoute('/')({
 function RouteComponent() {
 	return (
 		<div>
+			<nav>
+				<ul className='flex gap-4'>
+					<li>
+						<Link to='/'>Home</Link>
+					</li>
+					<li>
+						<Link to='/about'>About</Link>
+					</li>
+				</ul>
+			</nav>
+
 			<p>Hello "/"!</p>
 			<Button variant='default'>Shadcn/UI button</Button>
 		</div>
