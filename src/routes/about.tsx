939d217621e99602b0a2590f0feb150e2e9ce56b
Andres Court <andres@andrescourt.com>
2026-08-12 17:14:23 -0500

    feat(about): added contact link

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -15,6 +15,9 @@ function RouteComponent() {
                     <li>
                         <Link to='/about'>About</Link>
                     </li>
+                    <li>
+                        <Link to='/contact'>Contact</Link>
+                    </li>
                 </ul>
             </nav>
             <p>Hello "/about"!</p>
