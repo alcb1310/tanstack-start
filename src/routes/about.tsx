957ba1b695a07ec7701e8d7467baa990384ebdf5
Andres Court <andres@andrescourt.com>
2026-08-12 15:35:08 -0500

    feat(about): added navigation

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,9 +1,23 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from "@tanstack/react-router"
 
-export const Route = createFileRoute('/about')({
-  component: RouteComponent,
+export const Route = createFileRoute("/about")({
+    component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/about"!</div>
+    return (
+        <div>
+            <nav>
+                <ul className='flex gap-3'>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/about'>About</Link>
+                    </li>
+                </ul>
+            </nav>
+            <p>Hello "/about"!</p>
+        </div>
+    )
 }
