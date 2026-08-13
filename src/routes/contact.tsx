6dc096b063903a075c70ec0ececcf08c8e62fce4
Andres Court <andres@andrescourt.com>
2026-08-12 21:28:06 -0500

    feat(contact): added feature flag protection

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,9 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router"
+import { featureFlags } from "@/validation/env"
 
-export const Route = createFileRoute('/contact')({
-  component: RouteComponent,
+export const Route = createFileRoute("/contact")({
+    component: RouteComponent,
+    beforeLoad: () => {
+        if (!featureFlags.contactFlag) {
+            throw new Error(
+                JSON.stringify({
+                    message: "Not implemented",
+                    code: 503,
+                    desc: "Service unavailable",
+                }),
+            )
+        }
+    },
 })
 
 function RouteComponent() {
-  return <div>Hello "/contact"!</div>
+    return <div>Hello "/contact"!</div>
 }
