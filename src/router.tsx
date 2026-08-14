fc867e8174d0f2e14607c3c64a7c6781cea53e27
Andres Court <andres@andrescourt.com>
2026-08-13 21:19:19 -0500

    feat(error): use default custom error component

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,15 @@ export function getRouter() {
 	const router = createRouter({
 		routeTree,
 		scrollRestoration: true,
+		defaultPreload: 'intent',
+		defaultErrorComponent: ({ error }) => {
+			return (
+				<div>
+					<h2>Global Error</h2>
+					<pre className='text-destructive'>{error.message}</pre>
+				</div>
+			)
+		},
 	})
 
 	return router
