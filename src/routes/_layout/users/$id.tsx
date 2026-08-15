cf21b05b8f71d3cb910fe29123747be0afab03b8
Andres Court <andres@andrescourt.com>
2026-08-14 22:05:51 -0500

    feat(users): validate path parameters

--- src/routes/_layout/users/$id.tsx
+++ src/routes/_layout/users/$id.tsx
@@ -2,6 +2,21 @@ import { createFileRoute, notFound } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_layout/users/$id')({
 	component: RouteComponent,
+	params: {
+		parse: (rawParams) => {
+			const id = Number.parseInt(rawParams.id, 10)
+			if (Number.isNaN(id)) {
+				throw new Error(JSON.stringify({ msg: 'Invalid user id' }))
+			}
+
+			return { id }
+		},
+	},
+	loader: ({ params: { id } }) => {
+		if (id === 0) {
+			throw notFound()
+		}
+	},
 })
 
 function RouteComponent() {
