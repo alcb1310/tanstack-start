f0bc7ad63d6692d3787e62bb6a3abc03dd18375b
Andres Court <andres@andrescourt.com>
2026-08-15 23:08:21 -0500

    feat(forms): added button props to form button

--- src/components/forms/FormButton.tsx
+++ src/components/forms/FormButton.tsx
@@ -1,18 +1,27 @@
+import type { ComponentProps } from 'react'
 import { useFormContext } from '@/hooks/form-context'
 import { Button } from '../ui/button'
 
-type FormButtonProps = {
+interface FormButtonProps extends ComponentProps<typeof Button> {
 	label?: string
 }
 
-export function FormButton({ label }: Readonly<FormButtonProps>) {
+export function FormButton({
+	label = 'Submit',
+	...props
+}: Readonly<FormButtonProps>) {
 	const form = useFormContext()
 
 	return (
 		<form.Subscribe selector={(state) => state.isSubmitting}>
 			{(isSubmitting) => (
-				<Button type='submit' variant='default' disabled={isSubmitting}>
-					{label ? label : 'Submit'}
+				<Button
+					type='submit'
+					variant='default'
+					disabled={isSubmitting}
+					{...props}
+				>
+					{label}
 				</Button>
 			)}
 		</form.Subscribe>
