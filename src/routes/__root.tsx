fedf1916e02e418172cfd5f51ec209f8fc0f5cfd
Andres Court <andres@andrescourt.com>
2026-08-16 15:52:48 -0500

    feat(query): setup Tanstack Query

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,15 @@
-import { createRootRoute, HeadContent, Scripts } from '@tanstack/react-router'
+import type { QueryClient } from '@tanstack/react-query'
+import {
+	createRootRouteWithContext,
+	HeadContent,
+	Scripts,
+} from '@tanstack/react-router'
 import type { ReactNode } from 'react'
 import appCss from '../styles.css?url'
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+	queryClient: QueryClient
+}>()({
 	head: () => ({
 		meta: [
 			{ charSet: 'utf-8' },
