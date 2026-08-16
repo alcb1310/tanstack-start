09b81bbb28d5a8ed134cf82a8f94fcbd89eee7ae
Andres Court <andres@andrescourt.com>
2026-08-16 12:23:34 -0500

    feat(query): get next error status

--- src/routes/_layout/jokes.tsx
+++ src/routes/_layout/jokes.tsx
@@ -11,6 +11,7 @@ type JokeQuery = {
 }
 
 async function getJoke() {
+	console.log('getting the joke')
 	const query = await fetch(ClientEnv.VITE_JOKES_API, {
 		method: 'GET',
 	})
@@ -25,25 +26,38 @@ async function getJoke() {
 export const Route = createFileRoute('/_layout/jokes')({
 	component: RouteComponent,
 	loader: async () => {
-		return getJoke()
+		return await getJoke()
 	},
 })
 
 function RouteComponent() {
 	const j = Route.useLoaderData()
 	const [joke, setJoke] = useState<JokeQuery>(j.joke)
+	const [loading, setLoading] = useState(false)
+	const [error, setError] = useState('')
+	const [hasError, setHasError] = useState(false)
+
+	async function getNextJoke() {
+		setLoading(true)
+		setError('')
+		setHasError(false)
+		try {
+			const j = await getJoke()
+			setJoke(j.joke)
+		} catch (e) {
+			setError(e.message)
+			setHasError(true)
+		}
+		setLoading(false)
+	}
 
 	return (
 		<div>
 			<h2 className='text-xl font-bold mb-2'>Daily Chuck Norris Joke</h2>
-			<p>{joke.value}</p>
+			{!hasError && <p>{joke.value}</p>}
+			{hasError && <p>An error occurred: {error}</p>}
 
-			<Button
-				onClick={async () => {
-					const j = await getJoke()
-					setJoke(j.joke)
-				}}
-			>
+			<Button onClick={getNextJoke} disabled={loading} size='xs'>
 				get next joke
 			</Button>
 		</div>
