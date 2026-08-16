19c3d4a81d05efa662e48a438cb9810a543fcafe
Andres Court <andres@andrescourt.com>
2026-08-16 12:04:57 -0500

    feat(query): fetch a new joke on page load

--- src/routes/_layout/jokes.tsx
+++ src/routes/_layout/jokes.tsx
@@ -1,9 +1,35 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { ClientEnv } from '@/validation/env'
+
+type JokeQuery = {
+	icon_url: string
+	id: string
+	url: string
+	value: string
+}
 
 export const Route = createFileRoute('/_layout/jokes')({
-  component: RouteComponent,
+	component: RouteComponent,
+	loader: async () => {
+		const query = await fetch(ClientEnv.VITE_JOKES_API, {
+			method: 'GET',
+		})
+
+		if (!query.ok) {
+			throw new Error('Error fetching new joke')
+		}
+
+		return { joke: (await query.json()) as JokeQuery }
+	},
 })
 
 function RouteComponent() {
-  return <div>Hello "/_layout/jokes"!</div>
+	const j = Route.useLoaderData()
+
+	return (
+		<div>
+			<h2 className='text-xl font-bold mb-2'>Daily Chuck Norris Joke</h2>
+			<p>{j.joke.value}</p>
+		</div>
+	)
 }
