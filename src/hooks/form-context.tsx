e66151a8d3639df0f91434b5f9ca423f12d776b1
Andres Court <andres@andrescourt.com>
2026-08-15 22:37:46 -0500

    feat(form): application form

--- src/hooks/form-context.tsx
+++ src/hooks/form-context.tsx
@@ -1,4 +1,17 @@
-import { createFormHookContexts } from '@tanstack/react-form'
+import { createFormHook, createFormHookContexts } from '@tanstack/react-form'
+import { FormButton } from '@/components/forms/FormButton'
+import { FormTextField } from '@/components/forms/FormTextField'
 
 export const { fieldContext, useFieldContext, formContext, useFormContext } =
 	createFormHookContexts()
+
+export const { useAppForm } = createFormHook({
+	fieldComponents: {
+		FormTextField,
+	},
+	formComponents: {
+		FormButton,
+	},
+	fieldContext,
+	formContext,
+})
