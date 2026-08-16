1d457c93b3b7272afbddb042c327d1b7747e93e9
Andres Court <andres@andrescourt.com>
2026-08-16 12:12:53 -0500

    feat(query): add a button to fetch a new joke

--- src/routes/_layout/jokes.tsx
+++ src/routes/_layout/jokes.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { useState } from 'react'
+import { Button } from '@/components/ui/button'
 import { ClientEnv } from '@/validation/env'
 
 type JokeQuery = {
@@ -8,28 +10,42 @@ type JokeQuery = {
 	value: string
 }
 
+async function getJoke() {
+	const query = await fetch(ClientEnv.VITE_JOKES_API, {
+		method: 'GET',
+	})
+
+	if (!query.ok) {
+		throw new Error('Error fetching new joke')
+	}
+
+	return { joke: (await query.json()) as JokeQuery }
+}
+
 export const Route = createFileRoute('/_layout/jokes')({
 	component: RouteComponent,
 	loader: async () => {
-		const query = await fetch(ClientEnv.VITE_JOKES_API, {
-			method: 'GET',
-		})
-
-		if (!query.ok) {
-			throw new Error('Error fetching new joke')
-		}
-
-		return { joke: (await query.json()) as JokeQuery }
+		return getJoke()
 	},
 })
 
 function RouteComponent() {
 	const j = Route.useLoaderData()
+	const [joke, setJoke] = useState<JokeQuery>(j.joke)
 
 	return (
 		<div>
 			<h2 className='text-xl font-bold mb-2'>Daily Chuck Norris Joke</h2>
-			<p>{j.joke.value}</p>
+			<p>{joke.value}</p>
+
+			<Button
+				onClick={async () => {
+					const j = await getJoke()
+					setJoke(j.joke)
+				}}
+			>
+				get next joke
+			</Button>
 		</div>
 	)
 }
