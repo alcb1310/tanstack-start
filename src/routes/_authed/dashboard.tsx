7514807ea68ad3e93146d0777e7d95f8a2051e0a
Andres Court <andres@andrescourt.com>
2026-08-22 12:23:48 -0500

    feat(auth): read auth information from context

--- src/routes/_authed/dashboard.tsx
+++ src/routes/_authed/dashboard.tsx
@@ -2,8 +2,25 @@ import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_authed/dashboard')({
 	component: RouteComponent,
+	beforeLoad: ({ context }) => {
+		console.log('dashboard before: ', context)
+	},
+	loader: ({ context }) => {
+		console.log('dashboard: ', context)
+		return { cookieValue: context.cookieValue }
+	},
 })
 
 function RouteComponent() {
-	return <div>Hello "/_authed/dashboard"!</div>
+	const data = Route.useLoaderData()
+
+	return (
+		<div>
+			<p>Hello "/_authed/dashboard"!</p>
+
+			<p className='text-green-400'>
+				User: <span className='text-amber-700'>{data.cookieValue.name}</span>
+			</p>
+		</div>
+	)
 }
